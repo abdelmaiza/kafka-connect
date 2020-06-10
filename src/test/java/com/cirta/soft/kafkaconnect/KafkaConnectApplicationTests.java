45dd67911657ec8de57789ba4d0b6d59eb9af0f3
amaiza <abdeldjallil.maiza@gmail.com>
2020-06-10 16:28:29 +0200

    kafka connect conf

---
+++ src/test/java/com/cirta/soft/kafkaconnect/KafkaConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cirta.soft.kafkaconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
